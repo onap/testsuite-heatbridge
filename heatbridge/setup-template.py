6d43fc337b62f82e8162256f8b8cf4d0b936ccbe
Gary Wu <gary.i.wu@huawei.com>
2017-08-24 11:17:44 -0700

    Allow merge job to deploy to Nexus Snapshots repo
    Change artifact version to -SNAPSHOT (with PEP 440
    verioning workarounds) to allow the Jenkins merge
    job to deploy the artifacts to Snapshots repo.
    
    Change-Id: I9aa359da6608696757836e51f63f3512fcd6de4e
    Issue-id: TEST-48
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- heatbridge/setup-template.py
+++ heatbridge/setup-template.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='${PROJECT_NAME}',            # This is the name of your PyPI-package.
-    version='${VERSION}',                          # Update the version number for new releases     
+    version='${VERSION}'.replace("-SNAPSHOT", ".dev0"),                          # Update the version number for new releases
     description='Script to input heat stack information into aai',    # Info about script
     install_requires=['python-novaclient','python-cinderclient','python-glanceclient', 'os_client_config', 'python-neutronclient', 'python-heatclient'], # what we need
     packages=['heatbridge'],       # The name of your scipts package
