92d31b0b17ca900325d0c80e54c320de41ef4af9
DR695H <dr695h@att.com>
2019-10-01 11:31:45 -0400

    remvoing ; and uneeded imports
    Issue-ID: INT-1292
    Change-Id: Ib444023db31866e436f43f804f9c38fa00e66b79
    Signed-off-by: DR695H <dr695h@att.com>

--- heatbridge/heatbridge/OpenstackContext.py
+++ heatbridge/heatbridge/OpenstackContext.py
@@ -1,20 +1,20 @@
 class OpenstackContext:
     """OpenstackContext is a simple class that holds the provided information that heatbridge uses."""
     
-    #this holds the info of the openstack clients
-    username = None;
-    password = None;
-    tenant = None;
-    region = None;
-    owner = None;
-    domain_id = None;
-    project_name = None;
+    # this holds the info of the openstack clients
+    username = None
+    password = None
+    tenant = None
+    region = None
+    owner = None
+    domain_id = None
+    project_name = None
 
     def __init__(self, username, password, tenant, region, owner, domain_id, project_name):
-        self.username = username;
-        self.password = password;
-        self.tenant = tenant;
-        self.region = region;
-        self.owner = owner;
-        self.domain_id = domain_id;
-        self.project_name = project_name;
+        self.username = username
+        self.password = password
+        self.tenant = tenant
+        self.region = region
+        self.owner = owner
+        self.domain_id = domain_id
+        self.project_name = project_name
