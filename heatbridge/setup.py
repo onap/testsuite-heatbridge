58bf891dffae71a766a89910e6fb01703faae8fa
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-03-06 10:18:56 +0100

    Update tox setup
    Setuptools' 'setup.py test' command produces deprecation
    warning so removing it. Tox is performing sdist anyway so
    it's usage was superfluous anyway.
    
    Removing setup-template.py as it's not needed.
    
    Adding pytest in place of deprecated setuptools 'test'
    routine and some placeholder for tests.
    
    Change-Id: Ib03b1664286a0bd0425bf2aabe2dddf27fb01e8e
    Issue-ID: INT-1427
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- heatbridge/setup.py
+++ heatbridge/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='heatbridge',            # This is the name of your PyPI-package.
-    version='0.2',                          # Update the version number for new releases     
+    version='0.2',                          # Update the version number for new releases
     description='Script to input heat stack information into aai',    # Info about script
     install_requires=['python-novaclient','python-cinderclient','python-glanceclient', 'os_client_config', 'python-neutronclient', 'python-heatclient'], # what we need
     packages=['heatbridge'],       # The name of your scipts package
